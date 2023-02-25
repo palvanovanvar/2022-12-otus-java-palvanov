1284babfcb35789bfbf008a805f5e2f4182000e6
palvanovanvar <palvanovanvar@gmail.com>
2023-02-25 22:11:37 +0300

    lesson1
    lesson1

---
+++ hw01-gradle/src/main/java/ru/lesson1/HelloOtus.java
@@ -0,0 +1,10 @@
+package ru.lesson1;
+
+import com.google.common.hash.HashCode;
+
+public class HelloOtus {
+
+    public static void main(String... args) {
+        System.out.println(HashCode.fromInt(33));
+    }
+}
