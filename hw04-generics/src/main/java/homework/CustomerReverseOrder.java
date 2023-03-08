a7d01ac5d0c280c10b22d8e8c8396c012bf12a4f
palvanovanvar <palvanovanvar@gmail.com>
2023-03-08 10:27:23 +0300

    hw04-generics

---
+++ hw04-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -0,0 +1,20 @@
+package homework;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class CustomerReverseOrder {
+
+    //todo: 2. надо реализовать методы этого класса
+    //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
+    LinkedList<Customer> customers = new LinkedList<>();
+
+    public void add(Customer customer) {
+        customers.push(customer);
+    }
+
+    public Customer take() {
+        return customers.pop();
+    }
+}
